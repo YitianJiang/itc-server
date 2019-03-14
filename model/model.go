170631cbdad379b207f9857be2adb8d284d45f67
kanghuaisong <kanghuaisong@bytedance.com>
2019-03-14 17:50:06 +0800

    go重构itc后台源码

--- model/model.go
+++ model/model.go
@@ -1,1 +1,12 @@
-package main
+package model
+
+type Struct_User struct {
+
+	Name           string
+	Email          string
+	Avatar         string
+	Employeenumber uint
+	Role_id        int
+	Groupid        int
+	Full_name      string
+}
