6c22843d23b03d3fb8b978cbf434ef6bb2aed708
fanjuan.xqp <fanjuan.xqp@bytedance.com>
2019-05-13 16:19:59 +0800

    bug fix

--- database/dal/RejCaseModel.go
+++ database/dal/RejCaseModel.go
@@ -125,7 +125,7 @@ func QueryByConditions(param map[string]string) (*[]rejListInfo,int,error){
 
 	page,err := strconv.Atoi(param["page"])
 	pageSize,err := strconv.Atoi(param["pageSize"])
-	db = db.Select("ID,app_id,app_name,rej_time,rej_reason,solution,pic_loc").Limit(pageSize).Offset((page-1)*pageSize)
+	db = db.Limit(pageSize).Offset((page-1)*pageSize)
 	var infos []rejCase
 	//db = db.Where(condition)
 	if err := db.Order("ID DESC").Find(&infos).Error; err != nil{
