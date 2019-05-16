c61e9f6c95754a5752b8ad84a0ce05f322627527
yinzhihong <yinzhihong@bytedance.com>
2019-05-16 15:02:46 +0800

    解决冲突

--- controllers/certificates.go
+++ controllers/certificates.go
@@ -2,16 +2,8 @@ package controllers
 
 import (
 	"bytes"
-	_const "code.byted.org/clientQA/itc-server/const"
-	"code.byted.org/clientQA/itc-server/database"
-	"code.byted.org/clientQA/itc-server/database/dal"
-	"code.byted.org/clientQA/itc-server/utils"
-	"code.byted.org/gopkg/logs"
-	"code.byted.org/gopkg/tos"
 	"context"
 	"encoding/json"
-	"fmt"
-	"github.com/gin-gonic/gin"
 	"io"
 	"io/ioutil"
 	"math/rand"
@@ -19,26 +11,31 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"reflect"
 	"strconv"
 	"strings"
 	"time"
+
+	_const "code.byted.org/clientQA/itc-server/const"
+	"code.byted.org/clientQA/itc-server/database/dal"
+	"code.byted.org/clientQA/itc-server/utils"
+	"code.byted.org/gopkg/logs"
+	"code.byted.org/gopkg/tos"
+	"github.com/gin-gonic/gin"
 )
 
 /*
 遍历证书，查询过期日期并提醒给拥有者
 */
-func CertificateController(c *gin.Context){
+func CertificateController(c *gin.Context) {
 	if certificateSet := dal.QueryCertificate(map[string]interface{}{}); len(*certificateSet) != 0 {
-
-		var expiredCertificates  []map[string]interface{}  //存放即将过期证书记录
+		var expiredCertificates []map[string]interface{} //存放即将过期证书记录
 		copTime := time.Now().Unix()
 		//遍历数据库，找到符合条件的记录
-		for _, certificate := range *certificateSet{
+		for _, certificate := range *certificateSet {
 			expireTime := certificate.ExpireTime
-			copExpire, _ := strconv.ParseInt(expireTime,10,64)
-			subTime :=  copExpire - copTime
-			if subTime <= 2592000000 && subTime > 0{
+			copExpire, _ := strconv.ParseInt(expireTime, 10, 64)
+			subTime := copExpire - copTime
+			if subTime <= 2592000000 && subTime > 0 {
 				appName := certificate.Appname
 				usage := certificate.Usage
 				creator := certificate.Creator
@@ -48,132 +45,134 @@ func CertificateController(c *gin.Context){
 				newMails = append(newMails, "gongrui")
 				newMails = append(newMails, "chenyujun")
 				itemMap := map[string]interface{}{
-					"appname":appName,
-					"usage":usage,
-					"larkPeople":newMails,
-					"expire_time":expireTime,
+					"appname":     appName,
+					"usage":       usage,
+					"larkPeople":  newMails,
+					"expire_time": expireTime,
 				}
 				expiredCertificates = append(expiredCertificates, itemMap)
 			}
 		}
 		//符合条件的证书给creator发lark提醒
-		for _, item := range expiredCertificates{
+		for _, item := range expiredCertificates {
 			appname := item["appname"]
 			usage := item["usage"]
 			//unix时间戳转换为北京时间，提示给用户
 			expireTime, _ := strconv.ParseInt(item["expire_time"].(string), 10, 64)
 			noticeExpireTime := time.Unix(expireTime, 0).Format("02/01/2006 15:04:05")
-
 			larkPeople := item["larkPeople"]
-			for _, people := range larkPeople.([]string){
-				larkMessage := "预审平台系统消息:\n应用:" + appname.(string) + "\n用途：" +  usage.(string) + "\n过期时间" + noticeExpireTime
+			for _, people := range larkPeople.([]string) {
+				larkMessage := "预审平台系统消息:\n应用:" + appname.(string) + "\n用途：" + usage.(string) + "\n过期时间" + noticeExpireTime
 				utils.LarkDingOneInner(people, larkMessage)
-				fmt.Println("people: %s, larkMessage: %s", people, larkMessage)
 			}
 		}
 	}
 	c.JSON(http.StatusOK, gin.H{
-		"message" : "success",
-		"errorCode" : 0,
-		"data" : "即将过期证书已经全部提醒给创建者啦～～～",
+		"message":   "success",
+		"errorCode": 0,
+		"data":      "即将过期证书已经全部提醒给创建者啦～～～",
 	})
 }
 
 /*
 数据库查询符合条件的证书记录，并分页返回数据
 前端传入参数：
-appName：
-type
-creator
-page
-pageSize
-appid
+appName：app名称
+type：   证书类型
+creator  创建者
+page     第几页
+pageSize 页面展示数量
+appid    APP ID
 */
 func GetCertificates(c *gin.Context) {
 	//获取用户信息
 	name, f := c.Get("username")
 	if !f {
 		c.JSON(http.StatusOK, gin.H{
-			"message" : "未获取到用户信息！",
-			"errorCode" : -1,
-			"data" : "未获取到用户信息！",
+			"message":   "未获取到用户信息！",
+			"errorCode": -1,
+			"data":      "未获取到用户信息！",
 		})
 		return
 	}
-	var certificate []dal.CertificateModel
-	connection, err := database.GetConneection()
-	if err != nil {
-		logs.Error("Connect to Db failed: %v", err)
-		return
+	queryMap := make(map[string]interface{})
+	//参数必填检验
+	if appName, isExit := c.GetQuery("appName"); isExit == true {
+		queryMap["appname"] = appName
 	}
-	defer connection.Close()
-
-	db := connection.Table(dal.CertificateModel{}.TableName()).LogMode(_const.DB_LOG_MODE)
-	if appName, isExit := c.GetQuery("appName"); isExit == true{
-		//中文添加特殊处理，暂时还是无法查询中文记录
-		appname := "%"
-		appname += string(appName[:])
-		appname += "%"
-		fmt.Println(reflect.TypeOf(appname))
-		db = db.Where("appname LIKE ?", appname)
+	if cerType, isExit := c.GetQuery("type"); isExit == true {
+		queryMap["type"] = cerType
 	}
-	if cerType, isExit := c.GetQuery("type"); isExit == true{
-		db = db.Where("type = ?", cerType)
+	if isSelectCreator, isExit := c.GetQuery("creator"); isExit == true && isSelectCreator == "on" {
+		queryMap["creator"] = name
 	}
-	if isSelectCreator, isExit := c.GetQuery("creator"); isExit == true && isSelectCreator == "on"{
-		db = db.Where("creator = ?", name)
+	if page, isExit := c.GetQuery("page"); isExit == true {
+		queryMap["page"] = page
 	}
-
-	page, _ := strconv.Atoi(c.Query("page"))
-	pageSize, _ := strconv.Atoi(c.Query("pageSize"))
-
-	if page > 0 && pageSize > 0 {
-		db = db.Limit(pageSize).Offset((page - 1) * pageSize)
+	if pageSize, isExit := c.GetQuery("pageSize"); isExit == true {
+		queryMap["pageSize"] = pageSize
 	}
-
-	if err := db.Find(&certificate).GetErrors(); err != nil {
-		for _, singleError := range err{
-			logs.Error(singleError.Error())
-		}
+	//查询符合条件的数据
+	certificate := dal.QueryLikeCertificate(queryMap)
+	if certificate != nil && len(*certificate) > 0 {
+		c.JSON(http.StatusOK, gin.H{
+			"message":   "OK!",
+			"errorCode": 0,
+			"data":      *certificate,
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"message":   "未查询到符合条件的数据!",
+			"errorCode": 0,
+			"data":      nil,
+		})
 	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"errorCode" : 0,
-		"data": certificate,
-	})
 }
 
 /*
 添加证书
 */
-func AddCertificate(c *gin.Context){
+func AddCertificate(c *gin.Context) {
 	name, f := c.Get("username")
 	if !f {
 		c.JSON(http.StatusOK, gin.H{
-			"errorCode" : -1,
-			"message" : "未获取到用户信息！",
+			"errorCode": -1,
+			"message":   "未获取到用户信息！",
 		})
 		return
 	}
-
+	//获取上传的文件
 	file, header, _ := c.Request.FormFile("certificate_file")
 	if file == nil {
 		c.JSON(http.StatusOK, gin.H{
-			"errorCode" : -1,
-			"message":"未选择上传的文件！",
+			"errorCode": -1,
+			"message":   "未选择上传的文件！",
 		})
 		logs.Error("未选择上传的文件！")
 		return
 	}
 	defer file.Close()
 	certificateFileName := header.Filename //获取证书名称
-	pem := c.PostForm("pem")        //是否需要返回pem_file标志
-
+	//获取参数
+	pem, isExitPem := c.GetPostForm("pem")
+	pass, isExitPass := c.GetPostForm("password")
+	fileType, isExitType := c.GetPostForm("type")
+	appName, isExitAppName := c.GetPostForm("appName")
+	appId, isExitAppId := c.GetPostForm("appId")
+	usage, isExitUsage := c.GetPostForm("application")
+	mails, isExitMails := c.GetPostForm("mails")
+	//判空处理
+	if isExitPem == false || isExitPass == false || isExitType == false || isExitAppName == false || isExitUsage == false || isExitMails == false || isExitAppId == false {
+		c.JSON(http.StatusOK, gin.H{
+			"errorCode": -1,
+			"message":   "上传文件参数填写不完整！",
+		})
+		logs.Error("上传文件参数填写不完整！")
+		return
+	}
 	//查询证书过期日期
-	response := func() (resp *http.Response){
-
-		upstreamUrl := "http://10.2.9.226:9527/query_certificate_expire_date"
-
+	response := func() (resp *http.Response) {
+		upstreamUrl := "http://10.2.9.226:9527/query_certificate_expire_date" //过期日期访问地址
 		body := &bytes.Buffer{}
 		writer := multipart.NewWriter(body)
 		part, err := writer.CreateFormFile("certificate", certificateFileName)
@@ -181,127 +180,133 @@ func AddCertificate(c *gin.Context){
 			logs.Error("访问过期日期POST请求create form file错误！", err.Error())
 		}
 		//这里的io.Copy实现,会把file文件都读取到内存里面，然后当做一个buffer传给NewRequest. 对于大文件来说会占用很多内存
-		if _, err = io.Copy(part, file); err != nil{
+		if _, err = io.Copy(part, file); err != nil {
 			logs.Error("访问过期日期POST请求复制文件错误！", err.Error())
 		}
-
-		if c.PostForm("type") == ".p12"{
+		if fileType == ".p12" {
 			var export_pem string
-			if pem == "on"{
+			if pem == "on" {
 				export_pem = "1"
-			}else{
+			} else {
 				export_pem = "0"
 			}
 			writer.WriteField("export_pem", export_pem)
-			writer.WriteField("pass", c.PostForm("password"))
+			writer.WriteField("pass", pass)
 		}
 		writer.WriteField("username", name.(string))
-		writer.WriteField("type", c.PostForm("type"))
+		writer.WriteField("type", fileType)
 		contentType := writer.FormDataContentType()
-		if err = writer.Close(); err!= nil{
+		if err = writer.Close(); err != nil {
 			logs.Error("关闭writer出错！", err.Error())
 		}
-		if response, err := http.Post(upstreamUrl, contentType, body); err != nil{
+		response, err := http.Post(upstreamUrl, contentType, body)
+		if err != nil {
 			logs.Error("获取证书过期信息失败！", err.Error())
-		}else{
-			return response
 		}
-		return
+		return response
 	}()
-
+	//返回response判空处理
+	if response == nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errorCode": -1,
+			"message":   "访问过期日期返回为空",
+		})
+		return
+	}
 	//得到证书日期，pem等信息后存入数据库
 	responseBody, err := ioutil.ReadAll(response.Body)
-	if err != nil{
-		logs.Error("访问处理失败！", err.Error())
+	if err != nil {
+		logs.Error("返回Response处理失败！", err.Error())
 		c.JSON(http.StatusUnauthorized, gin.H{
-			"errorCode" : -1,
-			"message": "证书处理失败！可能是证书本身的格式问题，或者是密码错误",
-		})
-	}
-
-	result := make(map[string]interface{})
-	json.Unmarshal(responseBody, &result)
-
-	if len(result) == 1{
-		c.JSON(http.StatusUnauthorized, gin.H{
-			"errorCode" : -1,
-			"message": "证书处理失败！可能是证书本身的格式问题，或者是密码错误",
+			"errorCode": -1,
+			"message":   "返回Response处理失败！",
 		})
 		return
 	}
-
+	result := make(map[string]interface{})
+	json.Unmarshal(responseBody, &result)
+	if len(result) == 1 {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"errorCode": -1,
+			"message":   "返回结果错误，没有证书过期日期信息",
+		})
+		return
+	}
 	//db Model
 	var certificateModel dal.CertificateModel
 	certificateModel.Creator = name.(string)
 	certificateModel.ExpireTime = strconv.FormatFloat(result["expire_time"].(float64), 'E', -1, 64)
-	certificateModel.Appname = c.PostForm("appName")
-	certificateModel.AppId, _ = strconv.Atoi(c.PostForm("appId"))
-	certificateModel.Usage = c.PostForm("application")
-	certificateModel.Mails = c.PostForm("mails")
-	certificateModel.Type = c.PostForm("type")
-	certificateModel.Password = c.PostForm("password")
+	certificateModel.Appname = appName
+	certificateModel.AppId, _ = strconv.Atoi(appId)
+	certificateModel.Usage = usage
+	certificateModel.Mails = mails
+	certificateModel.Type = fileType
+	certificateModel.Password = pass
 	certificateModel.CertificateFileName = certificateFileName
-
 	//如果pem=on，返回中包含pem_file字符串
-	if _, ok := result["pem_file"]; ok{
-		pem_file_name := strings.Split(certificateFileName, ".")[0] +".pem"
-
+	if _, ok := result["pem_file"]; ok {
+		pem_file_name := strings.Split(certificateFileName, ".")[0] + ".pem"
 		certificateModel.PemFile = result["pem_file"].(string)
 		certificateModel.PemFileName = pem_file_name
 	}
-
 	//证书上传到TOS，获取访问地址并存到数据库certificate_file那一列
-	localCertificatePath := "./"+ c.PostForm("appId") + "." + strings.Split(certificateFileName, ".")[1]
-	newFile, err := os.Create(localCertificatePath)   //本地创建临时文件
-	if err!= nil{
+	localCertificatePath := "./" + c.PostForm("appId") + "." + strings.Split(certificateFileName, ".")[1]
+	newFile, err := os.Create(localCertificatePath) //本地创建临时文件
+	if err != nil {
+		c.JSON(http.StatusCreated, gin.H{
+			"errorCode": -1,
+			"message":   "创建临时文件失败！",
+		})
 		logs.Error("创建临时失败！", err.Error())
+		return
 	}
 	certificateFile, _ := header.Open()
-	io.Copy(newFile, certificateFile)  //复制证书文件内容到临时文件
-
+	io.Copy(newFile, certificateFile) //复制证书文件内容到临时文件
 	//上传证书到TOS，并获取访问地址
 	fileTosUrl := uploadTos(localCertificatePath)
+	if fileTosUrl == "" {
+		c.JSON(http.StatusOK, gin.H{
+			"errorCode": -1,
+			"message":   "得到的tos存储地址为空!",
+		})
+		logs.Error("得到的tos存储地址为空!")
+		return
+	}
 	certificateModel.CertificateFile = fileTosUrl
-
-	if err = os.Remove(localCertificatePath); err != nil{  //删除本地创建的临时文件
+	if err = os.Remove(localCertificatePath); err != nil { //删除本地创建的临时文件
 		logs.Error("删除临时文件失败！", err.Error())
 	}
-
-	if dal.InsertCertificate(certificateModel){
+	if dal.InsertCertificate(certificateModel) {
 		c.JSON(http.StatusOK, gin.H{
-			"errorCode" : 0,
-			"message": "OK!",
+			"errorCode": 0,
+			"message":   "OK!",
 		})
-	}else{
+	} else {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"errorCode" : -1,
-			"message": "Failed!",
+			"errorCode": -1,
+			"message":   "数据库存储错误，新建证书失败！",
 		})
 	}
-
 }
 
 /*
 文件上传到TOS，并获取访问url
 */
 func uploadTos(path string) string {
-
 	var tosBucket = tos.WithAuth(_const.TOS_BUCKET_NAME, _const.TOS_BUCKET_KEY)
 	context, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
-
 	tosPutClient, err := tos.NewTos(tosBucket)
 	fileName := filepath.Base(path)
 	byte, err := ioutil.ReadFile(path)
 	if err != nil {
-		logs.Error("%s", "打开文件失败" + err.Error())
-
+		logs.Error("%s", "打开文件失败"+err.Error())
 	}
-	key := fmt.Sprint(time.Now().UnixNano()) + "_" + fileName
+	key := strconv.FormatInt(time.Now().UnixNano(), 10) + "_" + fileName
 	logs.Info("key: " + key)
 	err = tosPutClient.PutObject(context, key, int64(len(byte)), bytes.NewBuffer(byte))
 	if err != nil {
-		logs.Error("%s", "上传tos失败：" + err.Error())
+		logs.Error("%s", "上传tos失败："+err.Error())
 		return ""
 	}
 	domains := tos.GetDomainsForLargeFile("TT", path)
@@ -312,5 +317,3 @@ func uploadTos(path string) string {
 	logs.Info("returnUrl: " + returnUrl)
 	return returnUrl
 }
-
-
--- router.go
+++ router.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"code.byted.org/clientQA/itc-server/casemanage"
 	"code.byted.org/clientQA/itc-server/controllers"
 	"code.byted.org/clientQA/itc-server/detect"
 	"code.byted.org/clientQA/itc-server/middleware"
 	"code.byted.org/gin/ginex"
-	"code.byted.org/clientQA/itc-server/casemanage"
 )
 
-func InitRouter(r *ginex.Engine){
+func InitRouter(r *ginex.Engine) {
 
 	api := r.GroupEX("/api")
 	//二进制包检测回调接口
@@ -17,13 +17,13 @@ func InitRouter(r *ginex.Engine){
 	r.GET("/t/generateToken", detect.GetToken)
 
 	//查询被拒案例
-	api.GET("/casemanage/queryRejCases",casemanage.GetRejCasesByConditions)
+	api.GET("/casemanage/queryRejCases", casemanage.GetRejCasesByConditions)
 	//新增被拒案例
-	api.POST("/casemanage/addRejCase",casemanage.AddRejCase)
+	api.POST("/casemanage/addRejCase", casemanage.AddRejCase)
 	//删除被拒案例
-	api.POST("/casemanage/deleteRejCase",casemanage.DeleteRejCase)
+	api.POST("/casemanage/deleteRejCase", casemanage.DeleteRejCase)
 	//更新被拒案例
-	api.POST("/casemanage/updateRejCase",casemanage.EditRejCaseofSolution)
+	api.POST("/casemanage/updateRejCase", casemanage.EditRejCaseofSolution)
 
 	api.Use(middleware.JWTCheck())
 	{
@@ -71,16 +71,12 @@ func InitRouter(r *ginex.Engine){
 		api.POST("/lark/updateGroup", detect.UpdateLarkGroup)
 		//删除lark群配置
 		api.DELETE("/lark/deleteGroup", detect.DeleteGroupInfoById)
-<<<<<<< HEAD
 		//添加证书
 		api.POST("/certificate", controllers.AddCertificate)
 		//查询展示证书
 		api.GET("/certificates", controllers.GetCertificates)
 		//过期证书提醒
 		api.GET("/certificates/controller", controllers.CertificateController)
-
-=======
->>>>>>> master
 
 	}
 }
