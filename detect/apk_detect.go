40e66b2a8ee9b4888e01f42ea011b57755392cea
fanjuan.xqp <fanjuan.xqp@bytedance.com>
2019-06-18 14:22:36 +0800

    bugfix,无确认信息

--- detect/apk_detect.go
+++ detect/apk_detect.go
@@ -2169,6 +2169,7 @@ func GetDetectDetailOutInfo(details []dal.DetectContentDetail, c *gin.Context,me
 						if info["status"] != 0 {
 							keys2[keyInfo] = 1
 							confirmInfo.Key = keyInfo
+							confirmInfo.Status = info["status"].(int)
 							confirmInfo.Remark = info["remarks"].(string)
 							confirmInfo.Confirmer = info["confirmer"].(string)
 							confirmInfo.OtherVersion = info["version"].(string)
