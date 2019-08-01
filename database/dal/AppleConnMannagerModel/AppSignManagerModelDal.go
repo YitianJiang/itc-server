99508410123a24e5d8d61d8dc6e9a0552db5c27e
fanjuan.xqp <fanjuan.xqp@bytedance.com>
2019-08-01 10:58:01 +0800

    增加表struct定义+Dal操作go文件

---
+++ database/dal/AppleConnMannagerModel/AppSignManagerModelDal.go
@@ -0,0 +1,69 @@
+package devconnmanager
+
+import (
+	_const "code.byted.org/clientQA/itc-server/const"
+	"code.byted.org/clientQA/itc-server/database"
+	"code.byted.org/clientQA/itc-server/utils"
+	"fmt"
+	"github.com/astaxie/beego/logs"
+)
+
+/**
+数据库Insert操作
+条件：record对应的struct有TableName()方法
+*/
+func InsertRecord(record interface{}) error {
+	conn, err := database.GetConneection()
+	if err != nil {
+		utils.RecordError("Get DB Connection Failed: ", err)
+		return err
+	}
+	defer conn.Close()
+
+	insertError := conn.Debug().Create(record).Error
+	if insertError != nil {
+		utils.RecordError("Insert into DB Failed: ", insertError)
+		return insertError
+	}
+	return nil
+}
+
+/**
+查询操作，
+返回nil---查询fail，返回空数组--无相关数据
+*/
+func QueryAppAccountCert(queryData map[string]interface{}) *[]AppAccountCert {
+	conn, err := database.GetConneection()
+	if err != nil {
+		utils.RecordError("Get DB Connection Failed: ", err)
+		return nil
+	}
+	defer conn.Close()
+	db := conn.Table(AppAccountCert{}.TableName()).LogMode(_const.DB_LOG_MODE)
+	var result = make([]AppAccountCert, 0)
+	if err := db.Where(queryData).Find(&result).Error; err != nil {
+		utils.RecordError("查询 tt_app_account_cert失败，查询条件："+fmt.Sprint(queryData)+",errInfo：", err)
+		return nil
+	}
+	return &result
+}
+
+/**
+删除操作
+*/
+func DeleteAppAccountCert(queryData map[string]interface{}) error {
+	conn, err := database.GetConneection()
+	if err != nil {
+		utils.RecordError("Get DB Connection Failed: ", err)
+		return err
+	}
+	defer conn.Close()
+	db := conn.Table(AppAccountCert{}.TableName()).LogMode(_const.DB_LOG_MODE)
+	var t AppAccountCert
+	if err1 := db.Where(queryData).Delete(&t).Error; err1 != nil {
+		logs.Error("删除 tt_app_account_cert失败，删除条件："+fmt.Sprint(queryData)+",errInfo：%v", err1)
+		utils.RecordError("删除 tt_app_account_cert失败，删除条件："+fmt.Sprint(queryData)+",errInfo：", err1)
+		return err1
+	}
+	return nil
+}
