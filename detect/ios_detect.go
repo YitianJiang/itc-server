f1811ed3b9d4d28548380846ac3c08feb9100567
阴志红 <yinzhihong@bytedance.com>
2019-08-08 20:24:31 +0800

    ios

--- detect/ios_detect.go
+++ detect/ios_detect.go
@@ -838,45 +838,40 @@ func GetIOSSelfNum(appid, taskId int) (bool, int) {
 //confirmLark 0:检测完成diff时，1：确认检测结果，2：确认自查结果
 func StatusDeal(detect dal.DetectStruct, confirmLark int) error {
 	//ci回调
-	if detect.Platform == 0 && detect.Status == 1 {
+	if detect.Status == 1 && (detect.Platform == 0 || detect.SelfCheckStatus == 1) {
 		if err := CICallBack(&detect); err != nil {
 			logs.Error("回调ci出错！", err.Error())
 			return err
 		}
-	} else if detect.SelfCheckStatus == 1 && detect.Status == 1 {
-		if err := CICallBack(&detect); err != nil {
-			logs.Error("回调ci出错！", err.Error())
-			return err
+		//diff时调用，不用发冗余消息提醒
+		if confirmLark == 0 {
+			return nil
 		}
+		//结果通知
+		go func() {
+			selfNoPass := detect.SelftNoPass
+			detectNoPass := detect.DetectNoPass
+			message := "你好，" + detect.AppName + " " + detect.AppVersion
+			if detect.Platform == 0 {
+				message += " Android包"
+			} else {
+				message += " iOS包"
+			}
+			message += "  已经确认完毕！"
+			url := "http://rocket.bytedance.net/rocket/itc/task?biz=" + detect.AppId + "&showItcDetail=1&itcTaskId=" + strconv.Itoa(int(detect.ID))
+			lark_people := detect.ToLarker
+			peoples := strings.Replace(lark_people, "，", ",", -1)
+			lark_people_arr := strings.Split(peoples, ",")
+			for _, p := range lark_people_arr {
+				utils.LarkConfirmResult(strings.TrimSpace(p), message, url, detectNoPass, selfNoPass, false)
+			}
+			lark_group := detect.ToGroup
+			groups := strings.Replace(lark_group, "，", ",", -1)
+			lark_group_arr := strings.Split(groups, ",")
+			for _, g := range lark_group_arr {
+				utils.LarkConfirmResult(strings.TrimSpace(g), message, url, detectNoPass, selfNoPass, true)
+			}
+		}()
 	}
-	//diff时调用，不用发冗余消息提醒
-	if confirmLark == 0 {
-		return nil
-	}
-	//结果通知
-	go func() {
-		selfNoPass := detect.SelftNoPass
-		detectNoPass := detect.DetectNoPass
-		message := "你好，" + detect.AppName + " " + detect.AppVersion
-		if detect.Platform == 0 {
-			message += " Android包"
-		} else {
-			message += " iOS包"
-		}
-		message += "  已经确认完毕！"
-		url := "http://rocket.bytedance.net/rocket/itc/task?biz=" + detect.AppId + "&showItcDetail=1&itcTaskId=" + strconv.Itoa(int(detect.ID))
-		lark_people := detect.ToLarker
-		peoples := strings.Replace(lark_people, "，", ",", -1)
-		lark_people_arr := strings.Split(peoples, ",")
-		for _, p := range lark_people_arr {
-			utils.LarkConfirmResult(strings.TrimSpace(p), message, url, detectNoPass, selfNoPass, false)
-		}
-		lark_group := detect.ToGroup
-		groups := strings.Replace(lark_group, "，", ",", -1)
-		lark_group_arr := strings.Split(groups, ",")
-		for _, g := range lark_group_arr {
-			utils.LarkConfirmResult(strings.TrimSpace(g), message, url, detectNoPass, selfNoPass, true)
-		}
-	}()
 	return nil
 }
