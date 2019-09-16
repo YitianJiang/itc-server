d9de1c6c7976e6163db517462a63600e69365dd2
hejiahui <hejiahui.2019@bytedance.com>
2019-09-16 11:45:50 +0800

    Delete rotten code

--- database/dal/BundleIdManagerModel.go
+++ database/dal/BundleIdManagerModel.go
@@ -18,30 +18,16 @@ import (
 
 type P8fileManager struct {
 	gorm.Model
-	BundleId               string    `gorm:"column:bundle_id"                     json:"bundleId"`
-	P8StringInfo 		   string    `gorm:"column:p8_string"                     json:"p8String"`
+	BundleId     string `gorm:"column:bundle_id"                     json:"bundleId"`
+	P8StringInfo string `gorm:"column:p8_string"                     json:"p8String"`
 }
+
 //表名
 func (c P8fileManager) TableName() string {
 	return "tb_bundleid_manager"
 }
 
-//func InsertBundleId (bundleid BundleIdManager) bool {
-//	connection, err := database.GetDBConnection()
-//	if err != nil {
-//		logs.Error("Connect to Db failed: %v", err)
-//		return false
-//	}
-//	defer connection.Close()
-//	err = connection.Table(BundleIdManager{}.TableName()).LogMode(_const.DB_LOG_MODE).Create(&bundleid).Error
-//	if err != nil {
-//		logs.Error("添加bundle id失败!", err)
-//		return false
-//	}
-//	return true
-//}
-
-func InsertBundleIdAndP8 (p8info P8fileManager) bool {
+func InsertBundleIdAndP8(p8info P8fileManager) bool {
 	connection, err := database.GetDBConnection()
 	if err != nil {
 		logs.Error("Connect to Db failed: %v", err)
@@ -56,32 +42,32 @@ func InsertBundleIdAndP8 (p8info P8fileManager) bool {
 	return true
 }
 
-func SearchBundleIds () (*[]P8fileManager,bool) {
+func SearchBundleIds() (*[]P8fileManager, bool) {
 	connection, err := database.GetDBConnection()
 	var BundleIdsObjResponse []P8fileManager
 	if err != nil {
 		logs.Error("Connect to Db failed: %v", err)
-		return &BundleIdsObjResponse,false
+		return &BundleIdsObjResponse, false
 	}
 	defer connection.Close()
 	if err := connection.Table(P8fileManager{}.TableName()).LogMode(_const.DB_LOG_MODE).Find(&BundleIdsObjResponse).Error; err != nil {
 		logs.Error("查询Bundle ID列表失败!", err)
-		return &BundleIdsObjResponse,false
+		return &BundleIdsObjResponse, false
 	}
-	return &BundleIdsObjResponse,true
+	return &BundleIdsObjResponse, true
 }
 
-func SearchP8String(input map[string]interface{}) (*P8fileManager,bool) {
+func SearchP8String(input map[string]interface{}) (*P8fileManager, bool) {
 	connection, err := database.GetDBConnection()
 	var BundleIdsObjResponse P8fileManager
 	if err != nil {
 		logs.Error("Connect to Db failed: %v", err)
-		return &BundleIdsObjResponse,false
+		return &BundleIdsObjResponse, false
 	}
 	defer connection.Close()
 	if err := connection.Table(P8fileManager{}.TableName()).LogMode(_const.DB_LOG_MODE).Where(input).Find(&BundleIdsObjResponse).Error; err != nil {
 		logs.Error("查询Bundle ID列表失败!", err)
-		return &BundleIdsObjResponse,false
+		return &BundleIdsObjResponse, false
 	}
-	return &BundleIdsObjResponse,true
+	return &BundleIdsObjResponse, true
 }
