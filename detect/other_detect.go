3440ac6cf6b58e6f26134b6005b4c7ada408cee3
hejiahui <hejiahui.2019@bytedance.com>
2019-09-23 21:19:57 +0800

    Delete rotten code

--- detect/other_detect.go
+++ detect/other_detect.go
@@ -1126,10 +1126,7 @@ func getOtherDetectDetailAARP(c *gin.Context, infos *[]dal.OtherDetailInfoStruct
 aar任务检测问题记录
 */
 func OtherDetectTaskErrorHandle(taskId int, errCode string, errInfo string) error {
-	// var errStruct dal.ErrorStruct
-	// errStruct.ErrCode = errCode
-	// errStruct.ErrInfo = errInfo
-	// errBytes, _ := json.Marshal(errStruct)
+
 	errBytes, err := json.Marshal(map[string]interface{}{
 		"errCode": errCode,
 		"errInfo": errInfo})
