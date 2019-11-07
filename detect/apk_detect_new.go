c4bf43823255862dfbbd374168c9a6043a489cf5
hejiahui <hejiahui.2019@bytedance.com>
2019-11-07 17:18:23 +0800

    Delete rotten code

--- detect/apk_detect_new.go
+++ detect/apk_detect_new.go
@@ -34,6 +34,7 @@ func ParseResultAndroid(task *dal.DetectStruct, resultJson string, toolID int) (
 		return err, 0
 	}
 
+	// Handle the basic information and permission of apk and aab package.
 	for i := range result.Result {
 		if err := AppInfoAnalysis_2(task, result.Result[i].AppInfo,
 			&dal.DetectInfo{TaskId: int(task.ID), ToolId: toolID}, i); err != nil {
@@ -44,13 +45,6 @@ func ParseResultAndroid(task *dal.DetectStruct, resultJson string, toolID int) (
 
 	//遍历结果数组，并将每组检测结果信息插入数据库
 	for index, result := range result.Result {
-		// //检测基础信息解析
-		// if err := AppInfoAnalysis_2(task, result.AppInfo,
-		// 	&dal.DetectInfo{TaskId: int(task.ID), ToolId: toolID},
-		// 	index); err != nil {
-		// 	logs.Error("%s analysis package failed: %v", msgHeader, err)
-		// 	return err, 0
-		// }
 
 		//获取敏感方法和字符串的确认信息methodInfo,strInfos，为信息初始化做准备
 		methodInfo, strInfos, _, err := getIgnoredInfo_2(task.AppId, task.Platform)
