1b9d5dacb9fb4993f15e1ee1c88e4066cafdeec2
hejiahui <hejiahui.2019@bytedance.com>
2019-12-01 10:39:26 +0800

    Use boe database in test

--- database/db.go
+++ database/db.go
@@ -3,13 +3,10 @@ package database
 import (
 	"fmt"
 
-	"code.byted.org/clientQA/itc-server/conf"
 	_const "code.byted.org/clientQA/itc-server/const"
 	"code.byted.org/clientQA/itc-server/utils"
-	"code.byted.org/golf/ssconf"
 	dbconf "code.byted.org/gopkg/dbutil/conf"
 	"code.byted.org/gopkg/dbutil/gormdb"
-	"code.byted.org/gopkg/env"
 	"code.byted.org/gopkg/gorm"
 	"code.byted.org/gopkg/logs"
 )
@@ -22,24 +19,24 @@ var (
 // InitDB initialize the dboptional
 func InitDB() {
 
-	if env.IsBoe() {
-		//boedb
-		ssConf, _ := ssconf.LoadSsConfFile(conf.Configuration.MysqlConfigPath)
-		dboptional = dbconf.GetDbConf(ssConf, "itcserver", dbconf.Write)
-	} else {
-		//online
-		//线上采用mysql gdpr
-		var err error
-		dboptional, err = dbconf.GetDBOptionalByConsulName("toutiao.mysql.itcserver_write")
-		if err != nil {
-			logs.Error("mysql gdpr failed,%v", err)
-			for _, lark_people := range _const.LowLarkPeople {
-				utils.LarkDingOneInner(lark_people, "mysql gdpr failed！")
-			}
+	// if env.IsBoe() {
+	// boedb
+	// ssConf, _ := ssconf.LoadSsConfFile(conf.Configuration.MysqlConfigPath)
+	// dboptional = dbconf.GetDbConf(ssConf, "itcserver", dbconf.Write)
+	// } else {
+	//online
+	//线上采用mysql gdpr
+	var err error
+	dboptional, err = dbconf.GetDBOptionalByConsulName("toutiao.mysql.itcserver_write")
+	if err != nil {
+		logs.Error("mysql gdpr failed,%v", err)
+		for _, lark_people := range _const.LowLarkPeople {
+			utils.LarkDingOneInner(lark_people, "mysql gdpr failed！")
 		}
 	}
+	// }
 
-	//test
+	// //test
 	// ssConf, _ := ssconf.LoadSsConfFile(conf.Configuration.MysqlConfigPath)
 	// dboptional = dbconf.GetDbConf(ssConf, "qa_ee", dbconf.Write)
 }
